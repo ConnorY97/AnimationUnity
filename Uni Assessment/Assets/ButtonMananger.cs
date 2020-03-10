021ee9c239532879e8d10efe534321c221abd3f4
Connor <c_young97@live.com>
2020-03-10 12:33:02 +1100

    Initial commit
    Just so I have a copy in case something is wrong with the assignment.

---
+++ Uni Assessment/Assets/ButtonMananger.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Animations;
+
+
+
+public class ButtonMananger : MonoBehaviour
+{
+    public ParticleSystem m_particalSystem;
+
+    private bool m_buttonClicked = false;
+
+    public GameObject m_zombie;
+
+    public void onButtonClick()
+    {
+        if (!m_buttonClicked)
+            m_buttonClicked = true;
+        else
+            m_buttonClicked = false; 
+    }
+
+    public void Attack()
+    {
+        //if (!m_zombie.GetComponent<Animator>().GetBool("Attack"))
+        //m_zombie.GetComponent<Animator>().SetBool("Attack", true);
+        //else 
+        //    m_zombie.GetComponent<Animator>().SetBool("Attack", false); 
+        m_zombie.GetComponent<Animator>().SetTrigger("Attack2");
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_particalSystem.Play();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_buttonClicked && !m_particalSystem.isPlaying)
+            m_particalSystem.Play();
+        else if (!m_buttonClicked)
+            m_particalSystem.GetComponent<ParticleSystem>().Stop(false, ParticleSystemStopBehavior.StopEmitting);
+    }
+
+
+
+}
---
+++ Uni Assessment/Assets/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.UI; 
+
+public class Player : MonoBehaviour
+{
+    public GameObject m_zombie;
+
+    public float m_movementSpeed = 10f;
+    public float m_rotationSpeed = 100f; 
+
+    private Animator m_zombieAnimator = null; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_zombieAnimator = m_zombie.GetComponent<Animator>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate((Input.GetAxis("Vertical") * Time.deltaTime) * m_movementSpeed, 0f, 0f);
+        //transform.forward.Set((Input.GetAxis("Vertical") * Time.deltaTime), 0f, 0f); 
+        transform.Translate(Vector3.forward * (Input.GetAxis("Vertical") * Time.deltaTime) * m_movementSpeed);
+        transform.Rotate(0f, (Input.GetAxis("Horizontal") * Time.deltaTime) * m_rotationSpeed, 0f); 
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+        {
+            m_zombieAnimator.SetBool("Walking", true);
+        }
+        else
+            m_zombieAnimator.SetBool("Walking", false);
+    }
+}
